cdd905b2b9628f2c275ca71aeb38003d1e0d3a5f
HENRIQUE PALU DE JESUS <26176282@educ123.sp.senai.br>
2026-09-22 15:36:42 -0300

    fix: remocao

--- remocao.php
+++ remocao.php
@@ -2,96 +2,95 @@
 require_once __DIR__ . "/init.php";
 
 $id = null;
-$CodigoAtual = null; 
+$CodigoAtual = null;
 
-if($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id'])){
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['id'])) {
     $id = $_GET['id'];
     $CodigoAtual = $_SESSION['eventos'][$id];
 }
 ?>
 
 <html>
-    <head></head>
-    <body>
-        <h1>Remover Evento</h1>
-        <?php require_once __DIR__ . "/nav.php"; ?>
 
-        <ul>
-            <?php foreach($_SESSION['eventos'] as $chaveEvento => $evento): ?>
-                <li>
-                    <a href="remocao.php?id=<?= $chaveEvento ?>">
-                        <?= $evento['titulo'] ?>
-                    </a>
-                </li>
-                <?php endforeach; ?>
-        </ul>
+<head></head>
 
-        <?php if($id == null): ?>
-            <p>Selecione uma das noticias acima</p>
-        <?php else: ?>
+<body>
+    <h1>Remover Evento</h1>
 
-                <form action="remocao-processa-form.php" method="POST">
-                    <input type="text" name="id" id="id" value="<?= $id ?>" hidden>
+    <ul>
+        <?php foreach ($_SESSION['eventos'] as $chaveEvento => $evento): ?>
+            <li>
+                <a href="remocao.php?id=<?= $chaveEvento ?>">
+                    <?= $evento['titulo'] ?>
+                </a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 
-        <label for="id">ID:</label>
-        <input type="text" id="id" name="id" placeholder="Coloque o id" required value="<?= $CodigoAtual['id'] ?>">
+    <?php if ($id == null): ?>
+        <p>Selecione uma das noticias acima</p>
+    <?php else: ?>
 
-        <br>
-        <br>
+        <form action="remocao-processa-form.php" method="POST">
+            <input type="text" name="id" id="id" value="<?= $id ?>" hidden>
 
-        <label for="titulo">TÍTULO:</label>
-        <input type="text" id="titulo" name="titulo" placeholder="Coloque o Título" required value="<?= $CodigoAtual['titulo'] ?>">
+            <br>
+            <br>
 
-        <br>
-        <br>
+            <label for="titulo">TÍTULO:</label>
+            <input type="text" id="titulo" name="titulo" placeholder="Coloque o Título" required value="<?= $CodigoAtual['titulo'] ?>">
 
-        <label for="descricao">Descrição:</label>
-        <input type="text" id="descricao" name="descricao" placeholder="Coloque a Descrição" required value="<?= $CodigoAtual['descricao'] ?>">
-        <br>
-        <br>
+            <br>
+            <br>
 
-        <label for="area">ÁREA:</label>
-        <input type="text" id="area" name="area" placeholder="area" required value="<?= $CodigoAtual['area'] ?>">
+            <label for="descricao">Descrição:</label>
+            <input type="text" id="descricao" name="descricao" placeholder="Coloque a Descrição" required value="<?= $CodigoAtual['descricao'] ?>">
+            <br>
+            <br>
 
-        <br>
-        <br>
+            <label for="area">ÁREA:</label>
+            <input type="text" id="area" name="area" placeholder="area" required value="<?= $CodigoAtual['area'] ?>">
 
-        <label for="data">DATA:</label>
-        <input type="text" id="data" name="data" placeholder="data" required value="<?= $CodigoAtual['data'] ?>">
+            <br>
+            <br>
 
-        <br>
-        <br>
+            <label for="data">DATA:</label>
+            <input type="text" id="data" name="data" placeholder="data" required value="<?= $CodigoAtual['data'] ?>">
 
-        <label for="inicio">INÍCIO:</label>
-        <input type="text" id="inicio" name="inicio" placeholder="inicio" required value="<?= $CodigoAtual['inicio'] ?>">
-        <br>
-        <br>
+            <br>
+            <br>
 
-        <label for="fim">FIM:</label>
-        <input type="text" id="fim" name="fom" placeholder="fim" required value="<?= $CodigoAtual['fim'] ?>">
+            <label for="inicio">INÍCIO:</label>
+            <input type="text" id="inicio" name="inicio" placeholder="inicio" required value="<?= $CodigoAtual['inicio'] ?>">
+            <br>
+            <br>
 
-        <br>
-        <br>
+            <label for="fim">FIM:</label>
+            <input type="text" id="fim" name="fom" placeholder="fim" required value="<?= $CodigoAtual['fim'] ?>">
 
-        <label for="local">LOCAL:</label>
-        <input type="text" id="local" name="local" placeholder="local" required value="<?= $CodigoAtual['local'] ?>">
+            <br>
+            <br>
 
-        <br>
-        <br>
+            <label for="local">LOCAL:</label>
+            <input type="text" id="local" name="local" placeholder="local" required value="<?= $CodigoAtual['local'] ?>">
 
-        <label for="responsavel">RESPONSÁVEL:</label>
-        <input type="text" id="responsavel" name="responsavel" placeholder="Coloque o responsavel" required value="<?= $CodigoAtual['responsavel'] ?>">
-        <br>
-        <br>
+            <br>
+            <br>
 
-        <br>
-        <br>
-        <button type="submit">Deletar</button>
+            <label for="responsavel">RESPONSÁVEL:</label>
+            <input type="text" id="responsavel" name="responsavel" placeholder="Coloque o responsavel" required value="<?= $CodigoAtual['responsavel'] ?>">
+            <br>
+            <br>
+
+            <br>
+            <br>
+            <button type="submit">Deletar</button>
 
 
 
-    </form>
-        <?php endif ?>        
+        </form>
+    <?php endif ?>
 
-    </body>
+</body>
+
 </html>
