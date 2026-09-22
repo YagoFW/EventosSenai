c645a75df0380a79b8770e3e5d2070ca83e3141d
YAGO FELIX WALCOW <26175356@educ123.sp.senai.br>
2026-09-22 16:47:11 -0300

    FIX: ajuste na nav bar

--- nav.php
+++ nav.php
@@ -2,5 +2,5 @@
     <a href="index.php">Home</a>
     <a href="cadastro.php">Formulário</a>
     <a href="edicao.php">Edição</a>
-    <a href="delete.php">Deletar Noticia</a>
+    <a href="remocao.php">Deletar Noticia</a>
 </nav>
